67708bdf518e6cda4087754887f59a48f102dfe8
KHA\kha <namkha87@gmail.com>
2017-02-10 21:22:48 +0700

    initial check in

---
+++ README.md
@@ -0,0 +1,24 @@
+# Data cleaning course project
+### The purpose of this script is to demonstrate the ability to collect, work with, and clean a data set.
+
+## What does the script do?
+1. Merges the training and the test sets to create one data set.
+2. Extracts only the measurements on the mean and standard deviation for each measurement.
+3. Uses descriptive activity names to name the activities in the data set
+4. Appropriately labels the data set with descriptive variable names.
+5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
+## How to use it?
+1. Download the data at [link](https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip "Dataset"). Extract it and keep the file names AS-IS
+2. Open the script *run_analysis.R* in RStudio or a text editor, make sure the *dirin* variable is point to the extracted folder in step 1, the *dirout* for the output dataset after cleaning
+`
+	dirin <-"yourpath/dataset"
+	dirout <- "yourpath/output"
+`
+3. Open the script *run_analysis.R* in RStudio and run:
+`
+	source("yourpath/run_analysis.R")
+`
+4. Verify if there are 2 below csv files generated in the output folder:
+ - merged.csv: for the 1st-4th requirement
+ - mean_by_activity_subject.csv: for the 5th requirement
---
+++ run_analysis.R
@@ -0,0 +1,60 @@
+## the data for project: https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
+## please keep the file names AS-IS after extracting
+
+## set the path containing input dataset and output
+dirin <-"c:/Code/Coursera/data-cleaning/project/dataset"
+dirout <- "c:/Code/Coursera/data-cleaning/project/output"
+
+## read the activity labels
+activitylabels <- read.table(paste0(dirin, "/activity_labels.txt"), header= F)
+
+## read features
+f <- read.table(paste0(dirin, "/features.txt"), header= F)
+f <- f[grep("mean\\(\\)", f[, 2]), ]
+features <- data.frame(id = f[, 1], featureName = f[, 2])
+
+## read the train dataset
+subtrain <- read.table(paste0(dirin, "/train/subject_train.txt"), header= F)[, 1]
+xtrain <- read.table(paste0(dirin, "/train/X_train.txt"), header= F)
+ytrain <- read.table(paste0(dirin, "/train/y_train.txt"), header= F)[, 1]
+
+## read the test dataset
+subtest <- read.table(paste0(dirin, "/test/subject_test.txt"), header= F)[, 1]
+xtest <- read.table(paste0(dirin, "/test/X_test.txt"), header= F)
+ytest <- read.table(paste0(dirin, "/test/y_test.txt"), header= F)[, 1]
+
+## a function to clean a dataset e.g. train, combine all data x, y, subject into a single data frame
+## basically it will solve these requirements: 2nd, 3rd, 4th
+mergeDataset <- function (x, y, subject) {
+    # remove all columns that are not measurements on the mean and standard deviation
+    x <- x[, features$id]
+    # set with descriptive variable names
+    colnames(x) <- features$featureName
+    
+    # set descriptive activity names
+    x$activity <- sapply(y, function (lblId) { activitylabels[lblId, 2] })
+    
+    # also add the test subject to dataset
+    x$subjectid <- subject
+    
+    # x now contains 35 columns dataset: 33 existing variables + 2 new columns
+    # we will reorder columns a little bit: 2 new columns move to first
+    x[, c(34, 35, 1:33)]
+}
+
+## now we merge each test set and train set
+mergedtest <- mergeDataset(xtest, ytest, subtest)
+mergedtrain <- mergeDataset(xtrain, ytrain, subtrain)
+
+## requirement 1st 2nd, 3rd, 4th: merge test and train together 
+merged <- rbind(mergedtest, mergedtrain)
+
+## finally 5th requirement
+meanmerged <- aggregate(x = merged[, 3:35], by = list(activity = merged$activity, subjectid = merged$subjectid), FUN = mean)
+
+## create the output directory if not exist
+if (!dir.exists(dirout)) dir.create(dirout)
+
+## write the output
+write.csv(merged, paste0(dirout, "/merged.csv"), row.names = F)
+write.csv(meanmerged, paste0(dirout, "/mean_by_activity_subject.csv"), row.names = F)
